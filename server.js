20670a8cb1af5531eda642f40e4c152b63272791
johan <johangunawan818@gmail.com>
2022-11-02 00:15:49 +0700

    add default route

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ if(process.env.NODE_ENV == 'development'){
 app.use(cors())
 app.use(json())
 
-app.use('/',(req,res) =>{
-    res.send('<h1>Welcome to MyGram</h1>')
-});
 app.use('/users',UserRouter);
 app.use('/photos',PhotoRouter);
 app.use('/socialmedias',SocialMediaRouter);
 app.use('/comments',CommentRouter);
+app.use('/',(req,res) =>{
+    res.send('<h1>Welcome to MyGram</h1>')
+});
 
 app.listen(process.env.PORT,() => {
     console.log("Server dah jalan di http://localhost:"+process.env.PORT || 5000);
