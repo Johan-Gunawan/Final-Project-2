47beafdf1adb60944339ef574a47c96c11d82505
johan <johangunawan818@gmail.com>
2022-11-15 22:31:10 +0700

    change platform deploy

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "#postgre123",
+    database: "final_project2",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres",
+  }
+}
